d45970d5d3d9b5510d531226edf6d1902a37c813
Bratah123 <brandonnguyen3301@gmail.com>
2023-05-15 20:53:29 -0700

    FIX:

--- pokemon_project.cpp
+++ pokemon_project.cpp
@@ -1,5 +1,4 @@
-
-//Group 8 
+// Group 8 
 #include <iostream>
 #include <cstdlib> //necessary for random numbers
 #include <ctime> 
@@ -14,23 +13,25 @@ int main()
 	int player_hp = 30;
 	int computer_hp = 30;
 
+	int nothing = 0;
+
 	//setting the stage
 	cout << "Welcome to fake pokemon! (press enter to continue)" << endl;
-	cin >> ;
+	cin >> nothing;
 	cout << "Your next target is to attack the pc with... uh..." << endl;
 	cout << "(press enter to continue)" << endl;
-	cin >> ;
+	cin >> nothing;
 	cout << "You shall be using your made up \"pokemon\"" << endl;
 	cout << "(press enter to continue)" << endl;
-	cin >>;
+	cin >> nothing;
 	cout << "These are the moves your pokemon has:" << endl;
 	cout << "1. Attack (this moves does "<< player_attack_power << " damage )" << endl;
 	cout << "2. Enrage (it boosts the next attack x2 but you skip your turn; enrage stacks)" << endl;
 	cout << "3. Block (it blocks all damage for the current turn)" << endl;
-	do{
+	do {
 	   cout << "Enter a number between 1-3 for your pokemon choice" << endl;
 	   cin >> player_choice;
-	   while (player_choice != 1 || player_choice != 2 || player_choice != 3 ){
+	   while (player_choice != 1 || player_choice != 2 || player_choice != 3 ) {
 		cout << "pls enter a number between 1-3..." << endl;
 		cin >> player_choice;
 	   }
