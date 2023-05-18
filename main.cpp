06aad8dbfe4cb00c77063aa012dcf925d131fca4
WSL Ubuntu <imbxyz1@gmail.com>
2023-05-18 11:09:46 -0700

    "terminal colors added"

--- main.cpp
+++ main.cpp
@@ -2,51 +2,60 @@
 #include <iostream>
 #include "pokemon/pokemon_factory.hpp"
 #include "pokemon/attack_factory.hpp"
+#include "pokemon/colors.hpp"
 
 // This function is used to allocate random Effort Values (EVs) to a player's Pokemon.
-void allocate_pokemon_random_evs(std::unique_ptr<Pokemon> & pokemon) {
+void allocate_pokemon_random_evs(std::unique_ptr<Pokemon> &pokemon)
+{
     int total_evs = 508;
-    while (total_evs > 0) {
+    while (total_evs > 0)
+    {
         int rand_ev = rand() % 200 + 1; // Generate a random number of EVs between 1 and the remaining total.
-        int rand_stat = rand() % 5; // Choose a random stat (out of 5 possible: health, base power, base special power, defense, special defense).
-        switch (rand_stat) {
-            case 0:
-                pokemon->add_health(rand_ev);
-                break;
-            case 1:
-                pokemon->add_base_power(rand_ev);
-                break;
-            case 2:
-                pokemon->add_base_special_power(rand_ev);
-                break;
-            case 3:
-                pokemon->add_def(rand_ev);
-                break;
-            case 4:
-                pokemon->add_spdef(rand_ev);
-                break;
-            default:
-                break;
+        int rand_stat = rand() % 5;     // Choose a random stat (out of 5 possible: health, base power, base special power, defense, special defense).
+        switch (rand_stat)
+        {
+        case 0:
+            pokemon->add_health(rand_ev);
+            break;
+        case 1:
+            pokemon->add_base_power(rand_ev);
+            break;
+        case 2:
+            pokemon->add_base_special_power(rand_ev);
+            break;
+        case 3:
+            pokemon->add_def(rand_ev);
+            break;
+        case 4:
+            pokemon->add_spdef(rand_ev);
+            break;
+        default:
+            break;
         }
         total_evs -= rand_ev;
     }
 }
 
-int main() {
+int main()
+{
     std::cout << "Welcome to Pokemon (Fake) Battle Simulator." << std::endl;
     std::cout << "The following Pokemon are available: " << std::endl;
     // Use pokemon factory to get list of options
     std::map<int, std::string>::const_iterator it = PokemonOptions::kOptions.begin();
-    while (it != PokemonOptions::kOptions.end()) {
+    while (it != PokemonOptions::kOptions.end())
+    {
         std::cout << "\tOption " << it->first << ": " << it->second << std::endl;
         ++it;
     }
     // Get user input for pokemon choice
-    std::cout << std::endl << "Please select your Pokemon: ";
+    std::cout << std::endl
+              << "Please select your Pokemon: ";
     int selection;
-    while (true) {
+    while (true)
+    {
         std::cin >> selection;
-        if (selection >= 1 && selection <= 8) {
+        if (selection >= 1 && selection <= 8)
+        {
             break;
         }
         std::cout << "Invalid selection. Please try again." << std::endl;
@@ -55,7 +64,7 @@ int main() {
     // Randomly select opponent's pokemon
     srand(time(NULL)); // Seed
     // int opponent_selection = (rand() % 7) + 1;  // note: this is a biased selection; to be replaced post-MVP
-    int opponent_selection = 2;  // temporarily hard code Charizard for testing
+    int opponent_selection = 2; // temporarily hard code Charizard for testing
 
     // Only the player & opponent pokemon will be instantiated
     auto players_pokemon = PokemonFactory::build(PokemonOptions::kOptions.at(selection));
@@ -67,7 +76,6 @@ int main() {
     players_pokemon->add_attack(AttackFactory::build("Ice Beam"));
     players_pokemon->add_attack(AttackFactory::build("Earthquake"));
 
-
     auto opponents_pokemon = PokemonFactory::build(PokemonOptions::kOptions.at(opponent_selection));
     allocate_pokemon_random_evs(opponents_pokemon);
 
@@ -77,54 +85,81 @@ int main() {
     opponents_pokemon->add_attack(AttackFactory::build("Thunder Punch"));
     opponents_pokemon->add_attack(AttackFactory::build("Earthquake"));
 
+    // Player colors
+    std::string PLAYER_COLOR = CYAN_TEXT;
+    std::string OPPONENT_COLOR = RED_TEXT;
+    std::string PLAYER_HP_COLOR = RESET_COLOR;
+    std::string OPPONENT_HP_COLOR = RESET_COLOR;
+
     std::cout << "--------------------------------------------" << std::endl;
-    std::cout << "You have selected " << players_pokemon->get_name() << "!" << std::endl;
-    std::cout << "Your opponent has chosen " << opponents_pokemon->get_name() << "!" << std::endl;
+    std::cout << "You have selected " << PLAYER_COLOR << players_pokemon->get_name() << "!" << RESET_COLOR << std::endl;
+    std::cout << "Your opponent has chosen " << OPPONENT_COLOR << opponents_pokemon->get_name() << "!" << RESET_COLOR << std::endl;
 
     // Battle Sequence
     bool has_not_ended = true;
     bool player_won;
-    while (has_not_ended) {
+
+    while (has_not_ended)
+    {
         // Terminal condition
-        if (Pokemon::has_fainted(players_pokemon, opponents_pokemon)) {
+        if (Pokemon::has_fainted(players_pokemon, opponents_pokemon))
+        {
             has_not_ended = false;
             player_won = players_pokemon->has_fainted() ? false : true;
             break;
         }
 
+        PLAYER_HP_COLOR = players_pokemon->get_health() >= 200 ? GREEN_TEXT : RED_TEXT;
+        OPPONENT_HP_COLOR = opponents_pokemon->get_health() >= 200 ? GREEN_TEXT : RED_TEXT;
+
         // Print out current game information, HP, etc
         std::cout << "--------------------------------------------" << std::endl;
-        std::cout << "Your Pokemon: " << players_pokemon->get_name() << std::endl;
-        std::cout << "HP: " << players_pokemon->get_health() << std::endl << std::endl;
-        std::cout << "Opponent's Pokemon: " << opponents_pokemon->get_name() << std::endl;
-        std::cout << "HP: " << opponents_pokemon->get_health();
+        std::cout << "Your Pokemon: " << PLAYER_COLOR << players_pokemon->get_name() << RESET_COLOR << std::endl;
+        std::cout << "HP: " << PLAYER_HP_COLOR << players_pokemon->get_health() << RESET_COLOR << std::endl
+                  << std::endl;
+        std::cout << "Opponent's Pokemon: " << OPPONENT_COLOR << opponents_pokemon->get_name() << RESET_COLOR << std::endl;
+        std::cout << "HP: " << OPPONENT_HP_COLOR << opponents_pokemon->get_health() << RESET_COLOR;
 
-        // TODO: Draw a box around attack selections like actual pokemon game
-        // 1 2
-        // 3 4
-        std::cout << std::endl << "--------------------------------------------" << std::endl;
-        for (int i = 0; i < players_pokemon->get_attacks().size(); i++) {
-            std::cout << "\t" << i + 1 << ". " << players_pokemon->get_attack(i)->get_name() << std::endl;
+        // Print Attack Options
+        std::cout << std::endl
+                  << "--------------------------------------------" << std::endl;
+        for (int i = 0; i < players_pokemon->get_attacks().size(); i++)
+        {
+
+            std::cout << "\t" << i + 1 << ". " << players_pokemon->get_attack(i)->get_name();
+
+            // new line every 2 elements.
+            // 1 2
+            // 3 4
+            if (i % 2 != 0)
+            {
+                std::cout << std::endl;
+            }
         }
+
         std::cout << "Please select an attack: ";
         int move_selection;
-        while (true) {
+        while (true)
+        {
             std::cin >> move_selection;
-            if (move_selection >= 1 && move_selection <= 4) {
+            if (move_selection >= 1 && move_selection <= 4)
+            {
                 break;
             }
             std::cout << "Invalid selection. Please try again." << std::endl;
         }
-        std::cout << std::endl << "--------------------------------------------" << std::endl;
-        std::cout << players_pokemon->get_name() << " has used " << players_pokemon->get_attack(move_selection - 1)->get_name() << "!" << std::endl;
+        std::cout << std::endl
+                  << "--------------------------------------------" << std::endl;
+        std::cout << PLAYER_COLOR << players_pokemon->get_name() << RESET_COLOR << " has used " << players_pokemon->get_attack(move_selection - 1)->get_name() << "!" << std::endl;
 
         // Calculate how much damage the player is dealing to the opponent
         int damage_to_deal = players_pokemon->CalcDamage(opponents_pokemon.get(), move_selection - 1);
-        std::cout << damage_to_deal << " damage dealt!" << std::endl;
+        std::cout << OPPONENT_COLOR << damage_to_deal << RESET_COLOR << " damage dealt!" << std::endl;
         opponents_pokemon->TakeDamage(damage_to_deal);
 
         // Terminal condition
-        if (Pokemon::has_fainted(players_pokemon, opponents_pokemon)) {
+        if (Pokemon::has_fainted(players_pokemon, opponents_pokemon))
+        {
             has_not_ended = false;
             player_won = players_pokemon->has_fainted() ? false : true;
             continue;
@@ -136,18 +171,22 @@ int main() {
         auto minimax = opponents_pokemon->MinimaxDecision(players_pokemon, 4, 1); // Use minimax to determine best move to use with a ply of 4
         int opponent_move_selection = minimax.second;
 
-        std::cout << "Opponent's " << opponents_pokemon->get_name() << " has used " << opponents_pokemon->get_attack(opponent_move_selection)->get_name() << "!" << std::endl;
+        std::cout << "Opponent's " << OPPONENT_COLOR << opponents_pokemon->get_name() << RESET_COLOR << " has used " << opponents_pokemon->get_attack(opponent_move_selection)->get_name() << "!" << std::endl;
         int damage_to_take = opponents_pokemon->CalcDamage(players_pokemon.get(), opponent_move_selection);
-        std::cout << damage_to_take << " damage dealt!" << std::endl;
+        std::cout << OPPONENT_COLOR << damage_to_take << RESET_COLOR << " damage dealt!" << std::endl;
         players_pokemon->TakeDamage(damage_to_take);
     }
 
-    if (player_won) {
-        std::cout << std::endl << "Congratulations!" << std::endl;
+    if (player_won)
+    {
+        std::cout << std::endl
+                  << "Congratulations!" << std::endl;
         std::cout << "You have won the battle against your Opponent's " << opponents_pokemon->get_name() << "." << std::endl;
     }
-    else {
-        std::cout << std::endl << "Aww, you lost! Better luck next time!" << std::endl;
+    else
+    {
+        std::cout << std::endl
+                  << "Aww, you lost! Better luck next time!" << std::endl;
     }
 
     return 0;
---
+++ pokemon/colors.hpp
@@ -0,0 +1,17 @@
+#include <string>
+
+#ifndef COLORS_HPP
+#define COLORS_HPP
+
+// ANSI escape sequences for text colors
+const std::string RESET_COLOR = "\033[0m";
+const std::string BLACK_TEXT = "\033[30m";
+const std::string RED_TEXT = "\033[31m";
+const std::string GREEN_TEXT = "\033[32m";
+const std::string YELLOW_TEXT = "\033[33m";
+const std::string BLUE_TEXT = "\033[34m";
+const std::string MAGENTA_TEXT = "\033[35m";
+const std::string CYAN_TEXT = "\033[36m";
+const std::string WHITE_TEXT = "\033[37m";
+
+#endif // COLORS_HPP
